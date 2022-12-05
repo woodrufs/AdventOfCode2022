94f4d1d652af98db560391ea5bbadc1789a4d680
Stephen Woodruff <swoodruff@plex.com>
2022-12-05 15:01:48 -0500

    Day 4

---
+++ src/day4/part1.ts
@@ -0,0 +1,21 @@
+import {readFileSync} from "fs";
+
+const exec = (fileName: string) => {
+  var elvesOverlapping = 0;
+  const lines = readFileSync(fileName).toString().split("\n");
+  const regEx = new RegExp(/(\d*)-(\d*),(\d*)-(\d*)/);
+  lines.forEach(line => {
+    const result = regEx.exec(line);
+    const elf1Start = Number(result[1]);
+    const elf1End = Number(result[2]);
+    const elf2Start = Number(result[3]);
+    const elf2End = Number(result[4]);
+    if (elf1Start<= elf2Start && elf1End >= elf2End
+        ||  elf2Start <= elf1Start && elf2End >= elf1End) {
+        elvesOverlapping++;
+    }
+  });
+  console.log(elvesOverlapping)
+};
+
+exec("dist/day4/input.txt");
